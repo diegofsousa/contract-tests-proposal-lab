d15960e73d8b5529c7e3ef08bf333681e61ebdca
diegofsousa <diegofernando5672@gmail.com>
2022-08-28 20:30:40 -0300

    [feature] schema test change 15 - test for fail - second stage

--- schema_tests/tests/schemas/messageV1.js
+++ schema_tests/tests/schemas/messageV1.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 module.exports = function (payload) {
     const schema = Joi.object().keys({
-        fee: Joi.number(),
+        fee: Joi.string(),
     }).required();
 
     return schema.validate(payload);
